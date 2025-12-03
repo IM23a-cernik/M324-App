a6e5a9945d7da0d26924fe0f8333a15d33c58b42
KrisHD1337 <kriscerni@gmail.com>
2025-12-03 14:37:04 +0100

    Added Docker stuff

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -1,24 +1,87 @@
-//import module traffic light
-const app = require("./trafficlight")
+//require supertest
+const request = require("supertest");
+//url of localserver is assigned to app
+const app = "http://localhost:3000";
 
-//test suite
-describe("Test suite traffic light page", ()=>{
-    it("case red light", ()=>{
-        expect(app.trafficLight("red")).toBe("DON'T WALK!")
-    })
-    it("case green light", ()=>{
-        expect(app.trafficLight("green")).toBe("WALK!")
-    })
-    it("case orange light", ()=>{
-        expect(app.trafficLight("orange")).toBe("ATTENTION!")
-    })
-    it("case out of order", ()=>{
-        expect(app.trafficLight(undefined)).toBe("OUT OF ORDER")
-    })
-})
-    //case red light
-//??
-//??
-//??
-//??
-//??
+describe("GET /api/welcome", () => {
+    it("welcome message -1", async () => {
+        return request(app)
+            .get("/api/welcome")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                if (!res.body.hasOwnProperty('status'))
+                    throw new Error("Expected 'status' key!");
+                if (!res.body.hasOwnProperty('message'))
+                    throw new Error("Expected 'message' key!");
+            })
+            .then((res) => {
+                expect(res.statusCode).toBe(200);
+            })
+    });
+
+    //this test fails
+    it("out of order (fail)", async () => {
+        return request(app)
+            .get("/api/trafficlight")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            //.* -> match any characters (Do not forget the point!)
+            .expect(/{"message":"Willkommen ...","status":"success"}/)
+    });
+
+    //this test succeeds
+    it("out of order (ok)", async () => {
+        return request(app)
+            .get("/api/trafficlight")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            //.* -> match any characters (Do not forget the point!)
+            .expect(/{"message":"OUT OF ORDER","status":"success"}/)
+    });
+
+    it("out of order (ok, but not advisable)", async () => {
+        return request(app)
+            .get("/api/welcome")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            //.* -> match any characters (Do not forget the point!)
+            .expect(/{"message":".*","status":"success"}/)
+    });
+});
+
+describe("GET /api/trafficlight", () => {
+    it("out of order", async () => {
+        return request(app)
+            .get("/api/trafficlight")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .expect(/{"message":"OUT OF ORDER","status":"success"}/)
+    });
+});
+
+describe("GET /api/trafficlight/:color", () => {
+    it("red", async () => {
+        return request(app)
+            .get("/api/trafficlight/red")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .expect(/{"message":"DON'T WALK!","status":"success"}/)
+    });
+
+    it("green", async () => {
+        return request(app)
+            .get("/api/trafficlight/green")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .expect(/{"message":"WALK!","status":"success"}/)
+    });
+
+    it("orange", async () => {
+        return request(app)
+            .get("/api/trafficlight/orange")
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .expect(/{"message":"ATTENTION!","status":"success"}/)
+    });
+});
+
